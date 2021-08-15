a2a85571048025d277796c17f267c234b1c7cb76
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-14 21:34:16 -0400

    Basic setup for CocktailList and SearchForm components

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,4 +1,17 @@
+import { useGlobalContext } from '../helpers/context';
+import Loading from './Loading';
+import Cocktail from './Cocktail';
+
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className='section-title'>No Cocktails Matched Your Search</h2>;
+  }
+
   return <div>Cocktail List Component</div>;
 };
 
--- src/helpers/context.js
+++ src/helpers/context.js
@@ -1,14 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        cocktails,
+        setSearchTerm,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-export const useGlobalState = () => {
-  useContext(AppContext);
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
